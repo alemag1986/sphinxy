84cd09e93bd62c9ba1ae384a4c92bdc81c23ff12
Alejandro Magnani <alejandro.magnani@gmail.com>
2023-04-20 15:48:06 -0600

    config

--- docs/conf.py
+++ docs/conf.py
@@ -47,3 +47,8 @@ html_theme = 'furo'
 html_static_path = ['_static']
 html_favicon = "_static/favicon.ico"
 
+# -- Sphinx Multiversion --------------------------------------------------
+# https://holzhaus.github.io/sphinx-multiversion/master/configuration.html#
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
+smv_branch_whitelist = r'^.*$'
+smv_remote_whitelist = r'^.*$'
